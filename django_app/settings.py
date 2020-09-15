58277131d9705bc87694ee10c68511e295e7b8da
Krystian Plich <krypli94@gmail.com>
2020-09-15 18:16:14 +0200

    Migrate DB to PostgreSQL

--- django_app/settings.py
+++ django_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_postgres',
+        'USER': os.environ.get('DJANGO_POSTGRE_USER'),
+        'PASSWORD': os.environ.get('DJANGO_POSTGRE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
